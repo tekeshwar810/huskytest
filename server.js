ed645af0bb987152f4d91e9b70692f6719899b86
wcg175 <tekeshwar810@gmail.com>
2023-09-29 15:48:47 +0530

    changes server

--- server.js
+++ server.js
@@ -1,6 +1,11 @@
-const express =                          require('express')
+const express =   
 
-const app                                                                = express()
+require('express')
+
+const app       
+
+
+= express()
 
 app.listen(3001,()=>{
     console.log('server is listen port 3001')
