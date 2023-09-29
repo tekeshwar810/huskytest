424c77beb2f18543b43f706a6481a21ebf380ca7
wcg175 <tekeshwar810@gmail.com>
2023-09-29 16:30:06 +0530

    chages

--- server.js
+++ server.js
@@ -1,5 +1,7 @@
-const express = require('express');
-const app = express();
+const express             =             require('express');
+const app =
+
+                    express();
 
 app.listen(3001, () => {
   console.log('server is listen port 3001');
